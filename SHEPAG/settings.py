547a4c4d19ebc3640ec3725db659463222fc7edc
Bethel-adjei <bethelobeng@gmail.com>
2024-11-26 13:07:21 +0000

    new branch

--- SHEPAG/settings.py
+++ SHEPAG/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ywwd6wh(yqz0yty1(5o4@)aydb9(z(j305726@9in6iz=^m)tg'
+SECRET_KEY = config("DJANGO_SECRETS_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,59 +33,57 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'shepag_app',
-    'blogapp',
-    
+    "jazzmin",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "shepag_app",
+    "blogapp",
     # rich editor
-        'django_summernote',
-
+    "django_summernote",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'SHEPAG.urls'
+ROOT_URLCONF = "SHEPAG.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR ,'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'SHEPAG.wsgi.application'
+WSGI_APPLICATION = "SHEPAG.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -93,16 +93,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -123,28 +123,32 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # or any other directory you prefer
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # or any other directory you prefer
 
 
-#Email settings
+# Email settings
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False  # Use TLS or SSL, not both
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'bethelobeng@gmail.com')  # Default email if env var not set
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'mrno vsps hbkx mepw')  # Default empty if env var not set
+EMAIL_HOST_USER = os.getenv(
+    "EMAIL_HOST_USER", "bethelobeng@gmail.com"
+)  # Default email if env var not set
+EMAIL_HOST_PASSWORD = os.getenv(
+    "EMAIL_HOST_PASSWORD", "mrno vsps hbkx mepw"
+)  # Default empty if env var not set
 
-DEFAULT_FROM_EMAIL = 'bethelobeng@gmail.com'
+DEFAULT_FROM_EMAIL = "bethelobeng@gmail.com"
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- shepag_app/admin.py
+++ shepag_app/admin.py
@@ -1,72 +1,103 @@
 from django.contrib import admin
-from .models import Product, BlogPost, Testimonial, ContactUs,TeamMembers,Subscriber,Newsletter
-from django_summernote.widgets import SummernoteWidget 
-from django.db import models 
+from .models import (
+    Product,
+    BlogPost,
+    Testimonial,
+    ContactUs,
+    TeamMembers,
+    Subscriber,
+    Newsletter,
+)
+from django_summernote.widgets import SummernoteWidget
+from django.db import models
 from django.utils.html import mark_safe
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from django.utils.html import strip_tags
-from django.core.mail import EmailMessage
 
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name',)  # Display name in the list view
-    search_fields = ('name', 'description')  # Add search functionality
+    list_display = ("name",)  # Display name in the list view
+    search_fields = ("name", "description")  # Add search functionality
 
-    formfield_overrides = { 
-            models.TextField: {'widget': SummernoteWidget}, 
-            
-     } 
+    formfield_overrides = {
+        models.TextField: {"widget": SummernoteWidget},
+    }
+
 
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'published_date', 'reading_time', 'display_image')
-    search_fields = ('title', 'content')
-    list_filter = ('published_date',)
+    list_display = ("title", "published_date", "reading_time", "display_image")
+    search_fields = ("title", "content")
+    list_filter = ("published_date",)
 
     def display_image(self, obj):
         if obj.image:
-            return mark_safe(f'<img src="{obj.image.url}" width="50" height="50" style="object-fit: cover;" />')
+            return mark_safe(
+                f'<img src="{obj.image.url}" width="50" height="50" style="object-fit: cover;" />'
+            )
         return "No Image"
 
-    display_image.short_description = 'Image'
+    display_image.short_description = "Image"
 
     formfield_overrides = {
-        models.TextField: {'widget': SummernoteWidget},
+        models.TextField: {"widget": SummernoteWidget},
     }
+
+
 class TestimonialAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date')  # Display name and date
-    search_fields = ('name', 'message')  # Add search functionality
+    list_display = ("name", "date")  # Display name and date
+    search_fields = ("name", "message")  # Add search functionality
+
 
 class ContactUsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'subject','message','sent_at')  # Display name, email, and phone
-    search_fields = ('name', 'email','')  # Add search functionality
-    list_filter = ('email',)  # Add a filter by email
+    list_display = (
+        "name",
+        "email",
+        "subject",
+        "message",
+        "sent_at",
+    )  # Display name, email, and phone
+    search_fields = ("name", "email", "")  # Add search functionality
+    list_filter = ("email",)  # Add a filter by email
 
 
 class TeamMembersAdmin(admin.ModelAdmin):
-    list_display = ('name', 'role', 'message', 'image')  # Fields to display in the list view
-    search_fields = ('name', 'role')  # Add search functionality
-    list_filter = ('role',)  # Add a filter for the 'role' field
-    fields = ('name', 'role', 'message', 'image')  # Explicitly show these fields in the form
+    list_display = (
+        "name",
+        "role",
+        "message",
+        "image",
+    )  # Fields to display in the list view
+    search_fields = ("name", "role")  # Add search functionality
+    list_filter = ("role",)  # Add a filter for the 'role' field
+    fields = (
+        "name",
+        "role",
+        "message",
+        "image",
+    )  # Explicitly show these fields in the form
 
-    formfield_overrides = { 
-            models.TextField: {'widget': SummernoteWidget}, 
-            
-     } 
-    
+    formfield_overrides = {
+        models.TextField: {"widget": SummernoteWidget},
+    }
+
+
 class SubscriberAdmin(admin.ModelAdmin):
-    list_display = ['email', 'subscribed_at']    
+    list_display = ["email", "subscribed_at"]
 
 
 class NewsletterAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')  # Display the 'title' and 'created_at' fields in the admin
+    list_display = (
+        "title",
+        "created_at",
+    )  # Display the 'title' and 'created_at' fields in the admin
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
 
         # Get all subscribers' email addresses
-        subscribers = Subscriber.objects.values_list('email', flat=True)
+        subscribers = Subscriber.objects.values_list("email", flat=True)
 
         # Strip HTML tags from the content (Summernote content)
         plain_message = strip_tags(obj.content)
@@ -82,7 +113,9 @@ class NewsletterAdmin(admin.ModelAdmin):
 
         # Add HTML content as an alternative to the plain-text message
         email.content_subtype = "html"  # Set the content type to HTML
-        email.attach_alternative(obj.content, "text/html")  # Attach the HTML version of the content
+        email.attach_alternative(
+            obj.content, "text/html"
+        )  # Attach the HTML version of the content
 
         # Try sending the email
         try:
@@ -92,8 +125,11 @@ class NewsletterAdmin(admin.ModelAdmin):
             print(f"Error sending newsletter: {e}")
 
     formfield_overrides = {
-        models.TextField: {'widget': SummernoteWidget},  # Use Summernote widget for text fields
+        models.TextField: {
+            "widget": SummernoteWidget
+        },  # Use Summernote widget for text fields
     }
+
 
 # Register the models with their custom admin classes
 admin.site.register(Product, ProductAdmin)
